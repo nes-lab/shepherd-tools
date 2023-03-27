6193bc79d61e3d8f0e84bdbb0fd1c570f7bd19e9
Ingmar Inʒo Splitt <orgua@users.noreply.github.com>
2023-03-27 11:21:10 +0200

    Update calibration.py

--- src/shepherd_data/calibration.py
+++ src/shepherd_data/calibration.py
@@ -7,7 +7,6 @@ from typing import TypeVar
 import numpy as np
 from numpy.typing import NDArray
 
-
 cal_default: Dict[str, Dict[str, float]] = {
     "voltage": {"gain": 3 * 1e-9, "offset": 0.0},  # allows 0 - 12 V in 3 nV-Steps
     "current": {"gain": 250 * 1e-12, "offset": 0.0},  # allows 0 - 1 A in 250 pA - Steps
