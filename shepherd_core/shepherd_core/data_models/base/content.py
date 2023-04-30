2f41781e55e5494b080b7d8c513bd35bd47bb86d
Ingmar Inʒo Splitt <orgua@users.noreply.github.com>
2023-04-30 19:00:00 +0200

    limit names to FS-compatible chars

--- shepherd_core/shepherd_core/data_models/base/content.py
+++ shepherd_core/shepherd_core/data_models/base/content.py
@@ -7,10 +7,14 @@ from pydantic import root_validator
 
 from .shepherd import ShpModel
 
-id_str = constr(to_lower=True, max_length=16, regex=r"^[\w]*$")
-name_str = constr(max_length=32, regex=r"^[ -~]*$")
-# ⤷ TODO: maybe this should be more limited to file-system-chars
-safe_str = constr(regex=r"^[ -~]*$")
+id_str = constr(to_lower=True, max_length=16, regex=r"^[\w]+$")
+name_str = constr(max_length=32, regex=r'^[^<>:;,?"*|]+$')
+safe_str = constr(regex=r"^[ -~]+$")
+
+# The Regex
+# [\w]          AlphaNum
+# [^<>:;,?"*|]  FS-Compatible ASCII
+# [ -~]         All Printable ASCII-Characters with Space
 
 
 class ContentModel(ShpModel):
@@ -27,16 +31,12 @@ class ContentModel(ShpModel):
     visible2group: bool = False
     visible2all: bool = False
 
-    # The Regex
-    # ^[\\w]*$    AlphaNum
-    # ^[ -~]*$    All Printable ASCII-Characters with Space
-
     @root_validator(pre=False)
     def content_validation(cls, values: dict):
         is_visible = values["visible2group"] or values["visible2all"]
         if is_visible and values["description"] is None:
             raise ValueError(
                 "Public instances require a description "
-                "(check open2*- and description-field)"
+                "(check visible2*- and description-field)"
             )
         return values
