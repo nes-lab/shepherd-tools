71d22f7f37a9b297323496eb7d4149728b0042af
Ingmar Inʒo Splitt <orgua@users.noreply.github.com>
2023-05-14 23:35:21 +0200

    ported most parts of harvester

--- shepherd_core/shepherd_core/data_models/content/virtual_harvester.py
+++ shepherd_core/shepherd_core/data_models/content/virtual_harvester.py
@@ -4,9 +4,12 @@ from pydantic import confloat
 from pydantic import conint
 from pydantic import root_validator
 
+from ...commons import samplerate_sps_default
 from ...logger import logger
+from ..base.calibration import CalibrationHarvester
 from ..base.content import ContentModel
 from ..base.fixture import Fixtures
+from ..base.shepherd import ShpModel
 from .energy_environment import EnergyDType
 
 fixture_path = Path(__file__).resolve().with_name("virtual_harvester_fixture.yaml")
@@ -24,7 +27,7 @@ class VirtualHarvester(ContentModel, title="Config for the Harvester"):
     datatype: EnergyDType = EnergyDType.ivsample
     # ⤷ of output
 
-    window_size: conint(ge=8, le=2_000) = 8  # TODO: min was 16
+    window_size: conint(ge=8, le=2_000) = 8  # TODO: min was 16, TEST
 
     voltage_mV: confloat(ge=0, le=5_000) = 2_500
     # ⤷ starting-point for some algorithms (mppt_po)
@@ -49,6 +52,9 @@ class VirtualHarvester(ContentModel, title="Config for the Harvester"):
     wait_cycles: conint(ge=0, le=100) = 1
     # ⤷ first cycle: ADC-Sampling & DAC-Writing, further steps: waiting
 
+    # internal states
+    inheritance_chain: list = []
+
     def __str__(self):
         return self.name
 
@@ -59,9 +65,113 @@ class VirtualHarvester(ContentModel, title="Config for the Harvester"):
         if values["name"] == "neutral":
             raise ValueError("Resulting Harvester can't be neutral")
         logger.debug("VHrv-Inheritances: %s", chain)
+        values[
+            "inheritance_chain"
+        ] = chain  # TODO: not clean for transitioning to testbed
+
         return values
 
     @root_validator(pre=False)
     def post_validation(cls, values: dict) -> dict:
+        if values["voltage_min_mV"] > values["voltage_max_mV"]:
+            raise ValueError("Voltage min > max")
+        if values["voltage_mV"] < values["voltage_min_mV"]:
+            raise ValueError("Voltage below min")
+        if values["voltage_mV"] > values["voltage_max_mV"]:
+            raise ValueError("Voltage above max")
+
         # TODO: port it over
+        cal = CalibrationHarvester()  # todo: as argument?
+        values["current_limit_uA"] = max(
+            10**6 * cal.adc_C_Hrv.raw_to_si(4), values["current_limit_uA"]
+        )
+
+        if "voltage_step_mV" not in values:
+            values["voltage_step_mV"] = (
+                abs(values["voltage_max_mV"] - values["voltage_min_mV"])
+                / values["window_size"]
+            )
+
+        values["voltage_step_mV"] = max(
+            10**3 * cal.dac_V_Hrv.raw_to_si(4), values["voltage_step_mV"]
+        )
+
         return values
+
+    def calc_hrv_mode(self, for_emu: bool) -> int:
+        return 1 * int(for_emu) + 2 * self.rising
+
+    def calc_algorithm_num(self, for_emu: bool) -> int:
+        num = 0
+        for base in self.inheritance_chain:
+            if base in algorithms:
+                num += algorithms[base]
+        if for_emu and num <= algorithms["ivcurve"]:
+            raise ValueError(
+                f"[{self.name}] Select valid harvest-algorithm for emulator, "
+                f"current usage = {self.inheritance_chain}",
+            )
+        elif num < algorithms["isc_voc"]:
+            raise ValueError(
+                f"[{self.name}] Select valid harvest-algorithm for harvester, "
+                f"current usage = {self.inheritance_chain}",
+            )
+        return num
+
+
+u32 = conint(ge=0, lt=2**32)
+
+
+# Currently implemented harvesters
+# NOTE: numbers have meaning and will be tested ->
+# - harvesting on "neutral" is not possible
+# - emulation with "ivcurve" or lower is also resulting in Error
+# - "_opt" has its own algo for emulation, but is only a fast mppt_po for harvesting
+algorithms = {
+    "neutral": 2**0,
+    "isc_voc": 2**3,
+    "ivcurve": 2**4,
+    "cv": 2**8,
+    "cv20": 2**8,
+    # "ci": 2**9, # is this desired?
+    "mppt_voc": 2**12,
+    "mppt_po": 2**13,
+    "mppt_opt": 2**14,
+}
+
+
+class VirtualHarvesterPRU(ShpModel):
+    algorithm: u32
+    hrv_mode: u32
+    window_size: u32
+    voltage_uV: u32
+    voltage_min_uV: u32
+    voltage_max_uV: u32
+    voltage_step_uV: u32
+    # ⤷ for window-based algo like ivcurve
+    current_limit_nA: u32
+    # ⤷ lower bound to detect zero current
+    setpoint_n8: u32
+    interval_n: u32
+    # ⤷ between measurements
+    duration_n: u32
+    # ⤷ of measurement
+    wait_cycles_n: u32
+    # ⤷ for DAC to settle
+
+    @classmethod
+    def from_vhrv(cls, data: VirtualHarvester, for_emu: bool = False):
+        return cls(
+            algorithm=data.calc_algorithm_num(for_emu),
+            hrv_mode=data.calc_hrv_mode(for_emu),
+            window_size=data.window_size,  # TODO: emu gets window_samples
+            voltage_uV=data.voltage_mV * 10**3,
+            voltage_min_uV=data.voltage_min_mV * 10**3,
+            voltage_max_uV=data.voltage_max_mV * 10**3,
+            voltage_step_uV=data.voltage_step_mV * 10**3,
+            current_limit_nA=data.current_limit_uA * 10**3,
+            setpoint_n8=min(255, data.setpoint_n * 2**8),
+            interval_n=data.interval_ms * samplerate_sps_default * 1e-3,
+            duration_n=data.duration_ms * samplerate_sps_default * 1e-3,
+            wait_cycles_n=data.wait_cycles,
+        )
--- shepherd_core/tests/data_models/test_content_fixtures.py
+++ shepherd_core/tests/data_models/test_content_fixtures.py
@@ -3,6 +3,7 @@ from shepherd_core.data_models.content.energy_environment import fixtures as fix
 from shepherd_core.data_models.content.firmware import Firmware
 from shepherd_core.data_models.content.firmware import fixtures as fix_firmware
 from shepherd_core.data_models.content.virtual_harvester import VirtualHarvester
+from shepherd_core.data_models.content.virtual_harvester import VirtualHarvesterPRU
 from shepherd_core.data_models.content.virtual_harvester import fixtures as fix_hrv
 from shepherd_core.data_models.content.virtual_source import VirtualSource
 from shepherd_core.data_models.content.virtual_source import VirtualSourcePRU
@@ -28,12 +29,16 @@ def test_experiment_fixture_vsrc():
     for fix in fix_src:
         vsrc = VirtualSource(name=fix["name"])
         VirtualSource(id=fix["id"])
-        VirtualSourcePRU.from_vsrc(vsrc)
+        VirtualSourcePRU.from_vsrc(vsrc, log_intermediate_node=False)
+        VirtualSourcePRU.from_vsrc(vsrc, log_intermediate_node=True)
 
 
 def test_experiment_fixture_vhrv():
     for fix in fix_hrv:
         if fix["name"] == "neutral":
             continue
-        VirtualHarvester(name=fix["name"])
+        vhrv = VirtualHarvester(name=fix["name"])
         VirtualHarvester(id=fix["id"])
+        VirtualHarvesterPRU.from_vhrv(vhrv, for_emu=False)
+        if int(fix["id"]) >= 1030:
+            VirtualHarvesterPRU.from_vhrv(vhrv, for_emu=True)
