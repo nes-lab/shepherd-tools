d96f57da9a9cec0b17958222723bcfee201c1d44
Ingmar Inʒo Splitt <orgua@users.noreply.github.com>
2023-05-16 14:25:37 +0200

    describe testbed-example

--- shepherd_core/examples/model_tester.py
+++ shepherd_core/examples/model_tester.py
@@ -1,6 +1,3 @@
-from datetime import datetime
-from datetime import timedelta
-
 from shepherd_core.data_models.content import EnergyEnvironment
 from shepherd_core.data_models.content import Firmware
 from shepherd_core.data_models.content import VirtualHarvester
@@ -11,10 +8,25 @@ from shepherd_core.data_models.task import TestbedTasks
 
 Experiment.dump_schema("experiment_schema.yaml")
 
+"""
+
+How to define an experiment:
+
+- within python (shown in this example)
+    - object-oriented
+    - sub-elements reusable
+    - scriptable for suite of experiments
+    - check for plausibility right away 
+- as yaml (shown in experiment_from_yaml.yaml)
+    - minimal writing
+    - easily copyable
+    - submittable through web-interface
+
+"""
+
 hrv = VirtualHarvester(name="mppt_bq_thermoelectric")
 
 target_cfgs = [
-    # first init similar to yaml
     TargetConfig(
         target_IDs=list(range(3001, 3004)),
         custom_IDs=list(range(0, 3)),
@@ -25,7 +37,7 @@ target_cfgs = [
     # second Instance fully object-oriented
     TargetConfig(
         target_IDs=list(range(2001, 2005)),
-        custom_IDs=list(range(7, 18)),
+        custom_IDs=list(range(7, 18)),  # note: longer list is OK
         energy_env=EnergyEnvironment(name="ThermoelectricWashingMachine"),
         virtual_source=VirtualSource(name="BQ25570-Schmitt", harvester=hrv),
         firmware1=Firmware(name="nrf52_demo_rf"),
@@ -33,19 +45,29 @@ target_cfgs = [
     ),
 ]
 
-xperi = Experiment(
+xperi1 = Experiment(
     id="4567",
     name="meaningful Test-Name",
-    time_start=datetime.utcnow() + timedelta(minutes=30),
+    # time_start=datetime.utcnow() + timedelta(minutes=30),
+    time_start="2033-03-13 14:15:16",
     target_configs=target_cfgs,
 )
 
-xperi.to_file("experiment_dict.yaml")
-
-xperi2 = Experiment.from_file("experiment_dict.yaml")
-
-print(f"xp1 hash: {xperi.get_hash()}")
+# Safe, reload and compare content
+xperi1.to_file("experiment_from_py.yaml")
+xperi2 = Experiment.from_file("experiment_from_py.yaml")
+print(f"xp1 hash: {xperi1.get_hash()}")
 print(f"xp2 hash: {xperi2.get_hash()}")
 
-tbt = TestbedTasks.from_xp(xperi2)
-tbt.to_file("experiment_tb_tasks.yaml")
+# comparison to same config (in yaml) fails due to internal variables, BUT:
+xperi3 = Experiment.from_file("experiment_from_yaml.yaml")
+print(f"xp3 hash: {xperi3.get_hash()} (won't match)")
+
+# Create a tasks-list for the testbed
+tb_tasks2 = TestbedTasks.from_xp(xperi2)
+tb_tasks2.to_file("experiment_tb_tasks.yaml")
+
+# Comparison between task-Lists succeed (experiment-comparison failed)
+tb_tasks3 = TestbedTasks.from_xp(xperi3)
+print(f"tasks2 hash: {tb_tasks2.get_hash()}")
+print(f"tasks3 hash: {tb_tasks3.get_hash()}")
