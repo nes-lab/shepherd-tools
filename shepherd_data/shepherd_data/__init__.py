c3bb85e7c1c7b027df289ed22c8c4af06c06d8e6
Ingmar Splitt <orgua@users.noreply.github.com>
2023-08-08 10:57:35 +0200

    Update __init__.py

--- shepherd_data/shepherd_data/__init__.py
+++ shepherd_data/shepherd_data/__init__.py
@@ -5,12 +5,11 @@ Provides classes for storing and retrieving sampled IV data to/from
 HDF5 files.
 
 """
-import shepherd_core
 from shepherd_core import BaseWriter as Writer
 
 from .reader import Reader
 
-__version__ = shepherd_core.__version__  # Packages are released together
+__version__ = "2023.8.2"
 
 __all__ = [
     "Reader",
