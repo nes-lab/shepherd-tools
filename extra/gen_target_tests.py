9c0119d7383bef0662f4a31fd57b045494df36c6
Ingmar Splitt <orgua@users.noreply.github.com>
2023-09-08 11:41:43 +0200

    improve generators

---
+++ extra/gen_target_tests.py
@@ -0,0 +1,92 @@
+"""
+
+
+"""
+from datetime import datetime
+from pathlib import Path
+
+import numpy as np
+
+from shepherd_core import BaseWriter as ShpWriter
+from shepherd_core import TestbedClient
+from shepherd_core.data_models.task import EmulationTask
+from shepherd_core.data_models.task import ObserverTasks
+from shepherd_core.data_models.task import ProgrammingTask
+from shepherd_core.data_models.testbed import ProgrammerProtocol
+from shepherd_core.data_models.testbed import TargetPort
+from shepherd_core.logger import logger
+
+
+def generate_lab_vsrc(path: Path):
+    # Config
+    voltage_V = 3.0
+    current_A = 50e-3
+    duration_s = 60
+
+    if path.exists():
+        logger.info("File exists, will skip: %s", path.name)
+    else:
+        with ShpWriter(path) as file:
+            file.store_hostname("artificial")
+            # values in SI units
+            timestamp_vector = np.arange(0.0, duration_s, file.sample_interval_ns / 1e9)
+            voltage_vector = np.linspace(
+                voltage_V, voltage_V, int(file.samplerate_sps * duration_s)
+            )
+            current_vector = np.linspace(
+                current_A, current_A, int(file.samplerate_sps * duration_s)
+            )
+            file.append_iv_data_si(timestamp_vector, voltage_vector, current_vector)
+
+
+if __name__ == "__main__":
+    path_cfg = Path("/etc/shepherd/")
+    path_rec = Path("/var/shepherd/recordings/")
+    path_here = Path(__file__).parent.absolute()
+    path_cnt = path_here / "content/"
+    path_pwr = path_here / "content/lab_pwr_src.h5"
+
+    tb_client = TestbedClient()
+    do_connect = False
+    if do_connect:
+        tb_client.connect()
+
+    # generate pwr-supply
+    generate_lab_vsrc(path_pwr)
+
+    # Self-test both ICs
+    tests = [
+        ("nrf52_testable", "msp430_testable", "target_device_test1"),
+        ("nrf52_rf_test", "msp430_deep_sleep", "target_device_test2"),
+        ("nrf52_deep_sleep", "msp430_deep_sleep", "target_device_test3"),
+    ]
+
+    for p_nrf, p_msp, name in tests:
+        path = ObserverTasks(
+            observer="sheep0",
+            owner_id=123,
+            time_prep=datetime.now(),
+            root_path=path_rec,
+            abort_on_error=False,
+            fw1_prog=ProgrammingTask(
+                firmware_file=path_cnt / p_nrf / "build.hex",
+                target_port=TargetPort.A,
+                mcu_port=1,
+                mcu_type="nRF52".lower(),
+                protocol=ProgrammerProtocol.SWD,
+            ),
+            fw2_prog=ProgrammingTask(
+                firmware_file=path_cnt / p_msp / "build.hex",
+                target_port=TargetPort.A,
+                mcu_port=2,
+                mcu_type="MSP430FR".lower(),
+                protocol=ProgrammerProtocol.SBW,
+            ),
+            emulation=EmulationTask(
+                input_path=path_pwr,
+                output_path=path_rec / (name + ".h5"),
+                power_tracing=None,
+                enable_io=True,
+            ),
+        ).to_file(path_cfg / name)
+        logger.info("Wrote: %s", path.as_posix())
