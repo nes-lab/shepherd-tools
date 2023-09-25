01b8856599b0793c3100178103e4b0914fe54a16
Ingmar Splitt <orgua@users.noreply.github.com>
2023-09-25 21:47:35 +0200

    Create gen_testbed_layout.py

---
+++ extra/gen_testbed_layout.py
@@ -0,0 +1,42 @@
+"""
+Allows to cross-check the relations between observer, cape and targets in each testbed
+"""
+from pathlib import Path
+
+from shepherd_core.data_models.testbed import Target, Testbed
+from shepherd_core.testbed_client.fixtures import Fixtures
+
+
+if __name__ == "__main__":
+    path_here = Path(__file__).parent.absolute()
+    separator = "; "  # excel parses this as table, but not ","?!?
+    with open(path_here / "content/testbed_layout.csv", "w") as csv:
+
+        elements = [
+            "tb_id", "tb_name", "target_id", "target_name",
+            "cape_id", "cape_name", "observer_id", "observer_name",
+            "room", "lat", "long",
+        ]
+        string = separator.join(elements)
+        csv.write(string + "\n")
+
+        for fix_tb in Fixtures()["Testbed"]:
+            tb = Testbed(id=fix_tb["id"])
+
+            for fix_tgt in Fixtures()["Target"]:
+                target = Target(id=fix_tgt["id"])
+
+                try:
+                    observer = tb.get_observer(target.id)
+                except ValueError:
+                    continue
+
+                cape = observer.cape
+
+                elements = [
+                    str(tb.id), tb.name, str(target.id), target.name,
+                    str(cape.id), cape.name, str(observer.id), observer.name,
+                    observer.room, str(observer.latitude), str(observer.longitude),
+                ]
+                string = separator.join(elements)
+                csv.write(string + "\n")
