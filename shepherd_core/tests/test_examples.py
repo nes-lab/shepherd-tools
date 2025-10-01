57f0dfac709fbc372d4fe56496e4acd5da6e2700
Ingmar Splitt <orgua@users.noreply.github.com>
2025-10-01 19:16:55 +0200

    clean up vectorized unit-tests (shows file-name now)

--- shepherd_core/tests/test_examples.py
+++ shepherd_core/tests/test_examples.py
@@ -8,43 +8,46 @@ import pytest
 path_example = Path(__file__).resolve().parent.parent / "examples"
 path_simulation = Path(__file__).resolve().parent.parent / "examples/simulations"
 
-simulations: list[Path] = [
-    path_simulation / "vharvester.py",
-    path_simulation / "vsource.py",
-    path_simulation / "vstorage.py",
+simulations: list[str] = [
+    "vharvester.py",
+    "vsource.py",
+    "vstorage.py",
 ]
 
 
-@pytest.mark.parametrize("path_file", simulations)
-def test_simulation_scripts(path_file: Path) -> None:
+@pytest.mark.parametrize("file", simulations)
+def test_simulation_scripts(file: str) -> None:
+    path_file = path_simulation / file
     os.chdir(path_file.parent)
     subprocess.run([sys.executable, path_file.as_posix()], shell=True, check=True)
 
 
-examples: list[Path] = [
-    path_example / "experiment_generic_var1.py",
-    path_example / "experiment_models.py",
-    path_example / "inventory.py",
-    path_example / "uart_decode_waveform.py",
-    path_example / "vsource_debug_sim.py",
+examples: list[str] = [
+    "experiment_generic_var1.py",
+    "experiment_models.py",
+    "inventory.py",
+    "uart_decode_waveform.py",
+    "vsource_debug_sim.py",
 ]
 
 
-@pytest.mark.parametrize("path_file", examples)
-def test_example_scripts(path_file: Path) -> None:
+@pytest.mark.parametrize("file", examples)
+def test_example_scripts(file: str) -> None:
+    path_file = path_example / file
     os.chdir(path_file.parent)
     subprocess.run([sys.executable, path_file.as_posix()], shell=True, check=True)
 
 
-examples_fw = [
-    path_example / "firmware_model.py",
-    path_example / "firmware_modification.py",
+examples_fw: list[str] = [
+    "firmware_model.py",
+    "firmware_modification.py",
 ]
 
 
 @pytest.mark.converter
 @pytest.mark.elf
-@pytest.mark.parametrize("path_file", examples_fw)
-def test_example_scripts_fw(path_file: Path) -> None:
+@pytest.mark.parametrize("file", examples_fw)
+def test_example_scripts_fw(file: str) -> None:
+    path_file = path_example / file
     os.chdir(path_file.parent)
     subprocess.run([sys.executable, path_file.as_posix()], shell=True, check=True)
