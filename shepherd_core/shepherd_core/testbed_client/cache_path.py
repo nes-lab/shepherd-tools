f5e4b1acc20852cc21cbc8f557bd9cb39ac574cd
Ingmar Splitt <orgua@users.noreply.github.com>
2025-11-18 15:06:16 +0100

    Update cache_path.py

--- shepherd_core/shepherd_core/testbed_client/cache_path.py
+++ shepherd_core/shepherd_core/testbed_client/cache_path.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 def _get_xdg_path(variable_name: str, default_path: Path) -> Path:
     value_ = os.environ.get(variable_name)
-    if value_ is None or value_ == "":
+    if not value_:  # catches None and ""
         return default_path
     return Path(value_)
 
