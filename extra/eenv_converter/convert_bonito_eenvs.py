d6a31988cf34b518e597e492fe56be1b3f106de5
Ingmar Splitt <orgua@users.noreply.github.com>
2026-01-21 18:52:50 +0100

    finish bonito metadata

--- extra/eenv_converter/convert_bonito_eenvs.py
+++ extra/eenv_converter/convert_bonito_eenvs.py
@@ -1,26 +1,34 @@
 """Script to convert Bonito recordings to Shepherd Nova EEnvs."""
 
 from collections.abc import Callable
+from datetime import datetime
+from datetime import timedelta
+from datetime import timezone
 from pathlib import Path
 from typing import Any
 
+import yaml
 from commons import process_mp
 from commons import root_storage_default
 from convert_v1_eenv import convert_file
+from shepherd_core.data_models import EnergyEnvironment
+from shepherd_core.data_models import EnergyProfile
+from shepherd_core.data_models import Wrapper
 from shepherd_core.logger import log
 
 # config
 
 bonito_input_path = Path().cwd() / "neslab-eh-data"
 
-DATASETS: dict[str, dict[str, str | int | dict]] = {
+DATASETS: dict[str, dict[str, Any]] = {
     "bonito_jogging_mixed": {
         "old_name": "jogging_mixed",
         # Omit sheep3 since its recording is broken
         # Its plot looks irregular compared to the other piezo harvesters.
         # A possible cause could be a bad contact.
         "node_glob": "sheep[01245]/jogging.h5",
-        "start": 1_621_081_752_600_000_000,  # TODO: iso-timestamp from this
+        "start": 1_621_081_752_600_000_000,
+        "timezone": timezone(timedelta(hours=2), "CEST"),
         "duration": int(3505.9 * 1e9),
         "description": (
             "Jogging tour of two people in a public park with solar and vibration harvesting. "
@@ -31,23 +39,24 @@ DATASETS: dict[str, dict[str, str | int | dict]] = {
         "metadata": {
             "location": "Europe, Germany, Berlin, H942+CX",
             "coordinates": "N 52.556585, E 13.3554427",
-            "timestamp": "2021-05-15T14:30+02",
             "route": "https://maps.app.goo.gl/qjWAJeZZfLec4cCE8 (there and back)",
-            "solar transducer": "IXYS KXOB25-05X3F",
-            "piezo transducer": "Mide S128-J1FR-1808YB",
+            "energy type": "light & kinetic",
             "energy source": "natural direct & indirect sun light, human body movement",
+            "transducer": "IXYS KXOB25-05X3F (PV), Mide S128-J1FR-1808YB (piezo)",
             "node0": "person 1, piezo, left ankle",
             "node1": "person 1, solar, left wrist",
             "node2": "person 1, piezo, right ankle",
             # sheep 3 scrapped
             "node3": "person 2, solar, left wrist",
             "node4": "person 2, piezo, right ankle",
+            # duration & TS are later derived from main data
         },
     },
     "bonito_stairs_solar": {
         "old_name": "data_step",
         "node_glob": "sheep*/rec_sheep*.h5",
         "start": 1_579_171_761_700_000_000,
+        "timezone": timezone(timedelta(hours=1), "CET"),
         "duration": int(3608.1 * 1e9),
         "description": (
             "Seven PV are embedded into an outdoor stair in front of a lecture hall. "
@@ -59,17 +68,19 @@ DATASETS: dict[str, dict[str, str | int | dict]] = {
         "metadata": {
             "location": "Europe, Germany, Dresden, Helmholtzstraße 18, Barkhausen-Bau",
             "coordinates": "N 51.026467, E 13.723217",
-            "timestamp": "2020-01-16T10:57:30+02",
-            "solar transducer": "KXOB25-05X3F-TR (no 100% guarantee)",
+            "energy type": "light",
             "energy source": "natural direct & indirect sun light",
-            "weather": "Sunny, 6°C",
+            "transducer": "KXOB25-05X3F-TR (PV, no 100% guarantee)",
+            "weather": "Sunny, around 6 degC",
             "weather-source": "https://meteostat.net/de/place/de/dresden?s=10488&t=2020-01-16/2020-01-16",
+            # duration & TS are later derived from main data
         },
     },
     "bonito_office_solar": {
         "old_name": "office_new",
         "node_glob": "sheep*/office_sd.h5",
         "start": 1_625_124_518_000_000_000,
+        "timezone": timezone(timedelta(hours=2), "CEST"),
         "duration": int(10798.0 * 1e9),
         "description": (
             "Five PV are mounted on doorframes and walls of an office and adjacent hallway. "
@@ -81,19 +92,22 @@ DATASETS: dict[str, dict[str, str | int | dict]] = {
         "metadata": {
             "location": "Europe, Germany, Dresden, Helmholtzstraße 18, Barkhausen-Bau",
             "coordinates": "N 51.026579, E 13.723024",
-            "solar transducer": "IXYS KX0B25-05X3F",
+            "energy type": "light",
             "energy source": "artificial fluorescent light, natural indirect sun light",
+            "transducer": "IXYS KX0B25-05X3F (PV)",
             "node0": "room",
             "node1": "room",
             "node2": "room",
             "node3": "hallway",
             "node4": "hallway",
+            # duration & TS are later derived from main data
         },
     },
     "bonito_cars_piezo": {
         "old_name": "cars_convoi",
         "node_glob": "sheep*/cars_convoi.h5",
         "start": 1_620_739_600_000_000_000,
+        "timezone": timezone(timedelta(hours=2), "CEST"),
         "duration": int(4687.3 * 1e9),
         "description": (
             "Two cars driving convoi over various roads from Windischleuba to Dresden. "
@@ -104,39 +118,44 @@ DATASETS: dict[str, dict[str, str | int | dict]] = {
         "metadata": {
             "location": "Europe, Germany, between Windischleuba & Dresden",
             "coordinates": "N 50.9914555, E 13.0990877",
-            "piezo transducer": "Mide S128-J1FR-1808YB",
-            "energy source": "cars",
+            "energy type": "kinetic",
+            "energy source": "car,motor,road",
+            "transducer": "Mide S128-J1FR-1808YB (piezo)",
             "node0": "following car, trunk",
             "node1": "leading car",
             "node2": "leading car",
             "node3": "following car, dashboard",
             "node4": "leading car",
             "node5": "following car, windshield",
+            # duration & TS are later derived from main data
         },
     },
     "bonito_washer_piezo": {
         "old_name": "washing_machine",
         "node_glob": "sheep*/washing_machine.h5",
         "start": 1_620_727_713_000_000_000,
+        "timezone": timezone(timedelta(hours=2), "CEST"),
         "duration": int(3880.2 * 1e9),
         "description": (
             "Five piezo harvesters are mounted on an industrial washing machine. "
-            "The model is called WPB4700H and it runs a 60°C washing program with maximum load. "
+            "The model is called WPB4700H and it runs a 60 degC washing program with maximum load. "
             "All described locations are from POV of looking at the front of the machine. "
-            "Trancducers move orthogonal to the surface they are put on. "
+            "Transducers move orthogonal to the surface they are put on. "
             "The nodes were powered by a POE Switch and synchronized via a GPS ref-clock."
         ),
         "metadata": {
             "location": "Europe, Germany, Rositz, 295M+PC",
             "coordinates": "N 51.0152002, E 12.3472641",
-            "piezo transducer": "Mide S128-J1FR-1808YB",
+            "energy type": "kinetic",
             "energy source": "industrial washing machine, WPB4700H",
+            "transducer": "Mide S128-J1FR-1808YB (piezo)",
             "node0": "top side - in the middle, slightly moved to front",
             "node1": "back side - top left corner (frontal POV)",
             "node2": "front side - top left corner",
             "node3": "right side - middle of top edge",
             "node4": "front side - in the middle on door",
             "node5": "back side - top right corner (frontal POV)",
+            # duration & TS are later derived from main data
         },
     },
     # Add a second version of the washer set as the beginning is rather
@@ -145,25 +164,28 @@ DATASETS: dict[str, dict[str, str | int | dict]] = {
         "old_name": "washing_machine",
         "node_glob": "sheep*/washing_machine.h5",
         "start": 1_620_729_193_200_000_000,
+        "timezone": timezone(timedelta(hours=2), "CEST"),
         "duration": int(2400.0 * 1e9),
         "description": (
             "Five piezo harvesters are mounted on an industrial washing machine. "
-            "The model is called WPB4700H and it runs a 60°C washing program with maximum load. "
+            "The model is called WPB4700H and it runs a 60 degC washing program with maximum load. "
             "All described locations are from POV of looking at the front of the machine. "
-            "Trancducers move orthogonal to the surface they are put on. "
+            "Transducers move orthogonal to the surface they are put on. "
             "The nodes were powered by a POE Switch and synchronized via a GPS ref-clock."
         ),
         "metadata": {
             "location": "Europe, Germany, Rositz, 295M+PC",
             "coordinates": "N 51.0152002, E 12.3472641",
-            "piezo transducer": "Mide S128-J1FR-1808YB",
+            "energy type": "kinetic",
             "energy source": "industrial washing machine, WPB4700H",
+            "transducer": "Mide S128-J1FR-1808YB (piezo)",
             "node0": "top side - in the middle, slightly moved to front",
             "node1": "back side - top left corner (frontal POV)",
             "node2": "front side - top left corner",
             "node3": "right side - middle of top edge",
             "node4": "front side - in the middle on door",
             "node5": "back side - top right corner (frontal POV)",
+            # duration & TS are later derived from main data
         },
     },
 }
@@ -209,5 +231,63 @@ def get_worker_configs(
     return cfgs
 
 
+def create_meta_data(path_dir: Path = root_storage_default) -> None:
+    """Generate a YAML containing the metadata for the dataset.
+
+    Combines data from hdf5-files itself and manually added descriptive data.
+    """
+    wraps = []
+    for new_name, params in DATASETS.items():
+        storage_dir = path_dir / "bonito" / new_name
+        log.info("Processing %s", storage_dir)
+        eprofiles: list[EnergyProfile] = []
+        for file_path in storage_dir.glob("*.h5"):
+            eprofile = EnergyProfile.derive_from_file(file_path)
+            data_update = {
+                # pretend data is available on server already (will be copied)
+                "data_path": Path("/var/shepherd/content/eenv/nes_lab/")
+                / file_path.relative_to(path_dir),
+                "data_2_copy": False,
+            }
+            eprofiles.append(eprofile.model_copy(deep=True, update=data_update))
+
+        if isinstance(params["metadata"], dict):
+            timestamp = datetime.fromtimestamp(params["start"] / 1e9, tz=params["timezone"])
+            duration = timedelta(seconds=params["duration"] / 1e9)
+            params["metadata"]["timestamp"] = timestamp.isoformat(timespec="milliseconds").rstrip(
+                "0:."
+            )
+            params["metadata"]["duration"] = str(duration).rstrip("0:.")
+            log.info(f"{new_name.ljust(30)}\t{timestamp}")
+
+        eenv = EnergyEnvironment(
+            name=new_name,
+            description=str(params["description"]),
+            metadata=params["metadata"],
+            # NOTE: intentionally fails if fields are missing
+            comment=f"created with {Path(__file__).name}",
+            energy_profiles=eprofiles,
+            owner="Ingmar",
+            group="NES_Lab",
+            visible2group=True,
+            visible2all=True,
+        )
+        wraps.append(
+            Wrapper(
+                datatype=EnergyEnvironment.__name__,
+                parameters=eenv.model_dump(exclude_none=True),
+            ).model_dump(exclude_unset=True, exclude_defaults=True)
+        )
+
+    wraps_yaml = yaml.safe_dump(
+        wraps,
+        default_flow_style=False,
+        sort_keys=False,
+    )
+    with (path_dir / "bonito/_metadata_bonito.yaml").open("w", encoding="utf-8-sig") as f:
+        f.write(wraps_yaml)
+
+
 if __name__ == "__main__":
     process_mp(get_worker_configs())
+    create_meta_data()
