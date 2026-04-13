adf25067400298da3c8c68e857cd9ae162c82cfb
Ingmar Splitt <orgua@users.noreply.github.com>
2026-04-13 20:01:34 +0200

    Update test_fixtures.py

--- shepherd_core/tests/testbed_client/test_fixtures.py
+++ shepherd_core/tests/testbed_client/test_fixtures.py
@@ -3,3 +3,4 @@ from shepherd_core.testbed_client.fixtures import Fixtures
 
 def test_validity_fixtures() -> None:
     Fixtures(reset=True, validate=True)
+    # TODO: could also instantiate complete models
